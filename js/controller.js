0924e76c27c4882aef6a452c1df5c18900245f36
rcasamajo <rcasamajo@iespoblenou.org>
2016-02-04 19:03:39 +0100

    Botons amb ng-repeat.

--- js/controller.js
+++ js/controller.js
@@ -6,7 +6,10 @@ app.controller('WebCalcController', function($scope) {
     $scope.res = 0;
     $scope.nums = [1, 4, 7];
 
-    $scope.numClick = function() {
-        $scope.res = 1000;
+    $scope.numClick = function(dgt) {
+        if ($scope.res == 0)
+            $scope.res = dgt;
+        else
+            $scope.res = $scope.res + "" + dgt;
     };
 });
