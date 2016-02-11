b95616baccf61184e9fb3afeb1cb1603259b79ad
rcasamajo <rcasamajo@iespoblenou.org>
2016-02-11 17:02:29 +0100

    Creant una directiva de prova.

---
+++ js/myDirective.js
@@ -0,0 +1,10 @@
+/**
+ * Created by rcasamajo on 11/02/16.
+ */
+
+app.directive('myDirective', function() {
+    return {
+        restrict: 'E',
+        templateUrl: 'myDirective.html',
+    };
+});
